b73f56a7cd50239d362d9646800caedf3d1a4264
Charlie2509 <charlie.southgate@macron.com>
2026-04-11 21:15:40 +0100

    Add order detail page

---
+++ app/orders/[id]/page.js
@@ -0,0 +1,8 @@
+export default function OrderDetail({ params }) {
+  return (
+    <main style={{ padding: 20 }}>
+      <h1>Order #{params.id}</h1>
+      <p>Details view coming next.</p>
+    </main>
+  );
+}
